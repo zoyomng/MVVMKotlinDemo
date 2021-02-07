1755628d5e7dfbac534d89aaaee7b02a8b4d8c52
zoyomng <zoyomng@163.com>
2021-02-07 18:21:41 +0800

    添加自定义控件

--- app/src/main/java/com/zoyo/mvvmkotlindemo/ui/view/SpiderView.java
+++
@@ -1,7 +0,0 @@
-package com.zoyo.mvvmkotlindemo.ui.view;
-
-/**
- * zoyomng 2021/2/6
- */
-public class SpiderView {
-}
---
+++ app/src/main/java/com/zoyo/mvvmkotlindemo/ui/view/spider/SpiderPointData.kt
@@ -0,0 +1,17 @@
+package com.zoyo.mvvmkotlindemo.ui.view.spider
+
+import android.graphics.Point
+
+/**
+ * zoyomng 2021/2/6
+ */
+class SpiderPointData(
+    x: Int,
+    y: Int,
+    val color: Int,
+    val r: Int,
+    var aX: Int,
+    var aY: Int,
+    val vX: Float,
+    val vY: Float
+) : Point(x, y)
